a82a27d4e7afd0ef33fa995ec363cd044410a4bd
Viktor Kushnir <mustbefail@gmail.com>
2024-11-15 19:01:22 +0200

    add client part, start with server

--- server/src/controllers/authController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-export const register = (req: Request, res: Response) => {
-  res.send('Register user');
-};
-
-export const login = (req: Request, res: Response) => {
-  res.send('Login user');
-};
--- server/src/routes/videoRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getTorrentInfo, streamTorrent } from '../controllers/videoController';
-
-const router = Router();
-
-router.post('/upload', getTorrentInfo);
-router.get('/stream/:infoHash', streamTorrent);
-
-export default router;
